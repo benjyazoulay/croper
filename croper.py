23f839f7cc7fb74ad547c0793cef5bfe3cfba4d4
Benjamin AZOULAY <25954316+benjyazoulay@users.noreply.github.com>
2024-10-30 15:00:42 +0100

    Update croper.py

--- croper.py
+++ croper.py
@@ -4,26 +4,14 @@ import numpy as np
 import os
 import zipfile
 import io
-import math
 
-def crop_image_with_overlap(image, size=1024):
+def crop_image(image, size=1024, overlap=128):
     width, height = image.size
     crops = []
-
-    # Calcul de l'overlap nécessaire pour chaque dimension
-    overlap_x = (width % size) if width % size != 0 else 0
-    overlap_y = (height % size) if height % size != 0 else 0
-
-    # Itération pour découper l'image en blocs carrés de 1024x1024 avec chevauchement si nécessaire
-    for top in range(0, height, size - overlap_y):
-        for left in range(0, width, size - overlap_x):
+    for top in range(0, height, size - overlap):
+        for left in range(0, width, size - overlap):
             box = (left, top, min(left + size, width), min(top + size, height))
             cropped_img = image.crop(box)
-            
-            # Redimensionnement si la tuile n'est pas exactement de 1024x1024
-            if cropped_img.size != (size, size):
-                cropped_img = cropped_img.resize((size, size), Image.ANTIALIAS)
-                
             crops.append(cropped_img)
     return crops
 
@@ -37,7 +25,7 @@ def save_crops_as_zip(crops):
     return zip_buffer
 
 def main():
-    st.title("Découper une image en haute résolution en tuiles de 1024x1024 avec chevauchement")
+    st.title("Découper une image en haute résolution en tuiles de 1024x1024 avec overlap")
 
     uploaded_file = st.file_uploader("Choisissez une image en haute résolution", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
@@ -45,7 +33,7 @@ def main():
         st.image(image, caption="Image originale", use_column_width=True)
 
         if st.button("Découper l'image"):
-            crops = crop_image_with_overlap(image, size=1024)
+            crops = crop_image(image, size=1024, overlap=128)
             st.write(f"Nombre d'images générées : {len(crops)}")
 
             zip_buffer = save_crops_as_zip(crops)
